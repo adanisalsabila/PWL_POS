920816426983a83f4cf4d0428234a9777d7dbdd5
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-03-05 09:10:25 +0700

    Praktikum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,9 +26,17 @@ class UserController extends Controller
         //     'level_id' => 4
         // ];
         // UserModel::insert($data);
-$data = ['nama' => 'Pelanggan Pertama', ];
+// $data = ['nama' => 'Pelanggan Pertama', ];
 
-UserModel::where('username', 'customer-1') -> update($data);
+$data =[
+    'level_id' => 2,
+    'username' => 'manager_dua',
+    'nama' => 'Manager 2',
+    'password' => Hash::make('12345')
+];
+UserModel::create($data);
+
+// UserModel::where('username', 'customer-1') -> update($data);
         $user = UserModel::all();
         return view('user',['data' => $user]);
     }
