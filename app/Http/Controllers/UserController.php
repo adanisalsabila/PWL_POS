62ec0c51bfa71edcd9a4481fa5926adc6664cb4a
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-03-05 09:25:50 +0700

    P4_Praktikum2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 // UserModel::create($data);
 
 // UserModel::where('username', 'customer-1') -> update($data);
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {abort(404);});
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user',['data' => $user]);
     }
 }
