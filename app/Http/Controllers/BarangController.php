62c0d05dfddd2da13745222b16e8354e77e16c4a
adanisalsabila <adanisalsabilaaa@gmail.com>
2025-03-19 16:52:31 +0700

    P6_Praktikum1

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use App\Models\BarangModel;
-use Illuminate\Support\Facades\Validator;
-use App\Models\UserModel;
+
 
 class BarangController extends Controller
 {
@@ -45,36 +44,5 @@ class BarangController extends Controller
 
         return redirect()->route('barang.index')->with('success', 'Barang berhasil dihapus');
     }
-    public function store_ajax(Request $request)
-{
-    // cek apakah request berupa ajax
-    if ($request->ajax() || $request->wantsJson()) {
-        $rules = [
-            'level_id' => 'required|integer',
-            'username' => 'required|string|min:3|unique:m_user,username',
-            'nama' => 'required|string|max:100',
-            'password' => 'required|min:6',
-        ];
-
-        // use Illuminate\Support\Facades\Validator;
-        $validator = Validator::make($request->all(), $rules);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false, // response status, false: error/gagal, true: berhasil
-                'message' => 'Validasi Gagal',
-                'msgField' => $validator->errors(), // pesan error validasi
-            ]);
-        }
-
-        UserModel::create($request->all());
-
-        return response()->json([
-            'status' => true,
-            'message' => 'Data user berhasil disimpan',
-        ]);
-    }
-
-    return redirect('/');
-}
+   
 }
